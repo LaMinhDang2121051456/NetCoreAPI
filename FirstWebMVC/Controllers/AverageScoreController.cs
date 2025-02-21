8fb088531afaa28d5dc097711c0eb185ff8df070
“LaMinhDang2121051456” <ptpmqlthayhien@gmail.com>
2025-02-21 11:23:19 +0700

    bai 2 ngay 19/02

--- FirstWebMVC/Controllers/AverageScoreController.cs
+++ FirstWebMVC/Controllers/AverageScoreController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.RegularExpressions;
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
@@ -9,15 +10,48 @@ namespace FirstWebMVC.Controllers
     public class AverageScore : Controller
     {
         [HttpGet]
-    public IActivationResult Index()
+    public IActionResult Index()
     {
         return View();
     }
 
     [HttpPost]
-    public IActivationResult Calculate(double Ascore, double Bscore, double Cscore, double TotalScore)
+    public IActionResult Calculate(String Ascore, String Bscore, String Cscore, String SubjectName)
     {
-        if ()
+        if (string.IsNullOrWhiteSpace(SubjectName) || string.IsNullOrWhiteSpace(Ascore) || string.IsNullOrWhiteSpace(Bscore) || string.IsNullOrWhiteSpace(Cscore)) 
+        {
+            ViewBag.Error = "ten mon hoc khong duoc trong!";
+            return View(); 
+        }
+        //kiem tra so hop le hay khomg(tu 0 den 10, 2 so thap phan)
+        string pattern = @"^\d+(\.\d{1,2})?$";
+        if (!Regex.IsMatch(Ascore, pattern) ||
+            !Regex.IsMatch(Bscore, pattern) ||
+            !Regex.IsMatch(Cscore, pattern)) 
+            {
+                ViewBag.Error = "Diem invalid, diem nam tu 0 den 10, toi da 2 so thap phan!";
+                return View("Index");
+            }
+        //chuyen sang so thuc 
+        double a = double.Parse(Ascore);
+        double b = double.Parse(Bscore);
+        double c = double.Parse(Cscore);  
+
+        //kiem tra diem co hop le ko 
+        if (a < 0 || a > 10 || b < 0 || b > 10 || c < 0 || c > 10)
+        {
+        ViewBag.Error = "diem phai nam trong khoang tu 0 den 10!";
+        return View("Index");   
+        }
+
+        //tinh diem theo cthuc 0.6a + 0.3b + 0.1c
+        double average = (a*0.6)+ (b*0.3)+(c*0.1);
+
+        //truyen du lieu sang view
+        ViewBag.SubjectName = SubjectName;  
+        ViewBag.Average = $"Diem cua mon {SubjectName}: {average:F2}";
+
+        return View("Result");
     }
     }
 }
--- FirstWebMVC/Models/AverageScore.cs
+++ FirstWebMVC/Models/AverageScore.cs
@@ -7,7 +7,7 @@ namespace FirstWebMVC.Models
     {
         [Required(ErrorMessage = "khong duoc de trong")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "ten mon hoc phai dai tu 3 den 100 ki tu")]
-        public string SujectName { get; set; }
+        public string SubjectName { get; set; } = "";
         [Required(ErrorMessage = "khong duoc de trong diem ")]
         [Range(0, 10, ErrorMessage = "diem nam tu 0 den 10")]
         public double Ascore { get; set; }
